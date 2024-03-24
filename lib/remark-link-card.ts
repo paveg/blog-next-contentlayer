7c2f998387f1fcd187217a4a5b38b7a04bb85565
Ryota Ikezawa <pavegy@gmail.com>
2024-03-24 14:44:19 +0900

    :construction: Create remark-link-card

---
+++ lib/remark-link-card.ts
@@ -0,0 +1,38 @@
+import { visit } from 'unist-util-visit';
+
+import type { Root } from 'mdast';
+import type { Plugin } from 'unified';
+import type { Parent } from 'unist';
+import { isBareLink, isParent } from './mdast/node-is';
+
+const LIST_ITEM = 'listItem';
+
+export const RemarkLinkCard: Plugin<void[], Root> = () => {
+  return (tree) => {
+    visit(tree, isBareLink, (node, _index, parent: Parent | undefined) => {
+      if (!isParent(parent)) {
+        return;
+      }
+
+      if (parent.type === LIST_ITEM) {
+        return;
+      }
+
+      const child = node.children[0];
+
+      if (!child.url.startsWith('http')) {
+        return;
+      }
+
+      child.data = {
+        ...child.data,
+        // TODO: fix
+        // hProperties: {
+        //   ...(child.data?.hProperties ?? {}),
+        //   dataLinkcard: true,
+        // },
+      };
+      console.log(child);
+    });
+  };
+};
